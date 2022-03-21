42adf71dbd0c6ee9da7df75f07b7367ce77c93f6
Carlos Pomares <cpomaresp@gmail.com>
2022-03-21 10:46:42 +0100

    Fixed entity on SDK and removed fetchers and models from SDK

--- sdk/src/datalake/sdk/entity.py
+++ sdk/src/datalake/sdk/entity.py
@@ -1,15 +1,8 @@
 from abc import abstractmethod
 
 class Entity(dict):
-
-    def __init__(self, data: dict):
-        self.__dict__.update(data)    
-    
-    def __str__(self) -> str:
-        return self.__class__.__name__ + f"({hex(id(self))})"
-    def __repr__(self) -> str:
-        return self.__class__.__name__ + f"({hex(id(self))})"
-
+    def __init__(self, *args, **kwargs):
+        super(Entity,self).__init__(*args, **kwargs)
     @abstractmethod
     def __schema__() -> dict:
         pass
