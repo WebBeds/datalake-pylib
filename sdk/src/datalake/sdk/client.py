3f078cb43511ffa8f53d5b8950a1265a5a42841a
Carlos Pomares <cpomaresp@gmail.com>
2022-03-21 13:18:13 +0100

    Added register as abstract and removed return annotation for get_resource

--- sdk/src/datalake/sdk/client.py
+++ sdk/src/datalake/sdk/client.py
@@ -1,7 +1,6 @@
 # SDK Client
-
+from abc import abstractmethod
 from .session import Session
-from .service import Service
 
 class Client:
 
@@ -12,13 +11,13 @@ class Client:
         self._session = session
         self.__register__()
 
-    def __register__(self) -> None:
-        # Hotel Service
-        from .fetchers.hotel import HotelFetcher
-        self._services['hotel'] = HotelFetcher(self._session)
-
-    def get_resource(self, name: str) -> Service:
-        return self._services[name]
-
     def available_services(self) -> list:
         return list(self._services.keys())
+
+    def get_resource(self, name: str):
+        return self._services[name]
+    
+    @abstractmethod
+    def __register__(self) -> None:
+        pass
+
