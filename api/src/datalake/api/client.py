3570046be34d40f4ffca55c086a7dd2d0d531b8f
Carlos Pomares <cpomaresp@gmail.com>
2022-03-21 13:07:19 +0100

    Added add/remove resources from API Client

--- api/src/datalake/api/client.py
+++ api/src/datalake/api/client.py
@@ -8,6 +8,18 @@ class APIClient(Client):
         # Master Fetcher
         self._services['master'] = MasterFetcher(self)
 
+    def add_resource(self, name: str, resource: object):
+        if name in self._services:
+            raise Exception(f"Resource '{name}' is already registered.")
+        self._services[name] = resource
+        return self
+
+    def remove_resource(self, name: str):
+        if name not in self._services:
+            raise Exception(f"Resource '{name}' is not registered.")
+        self._services.pop(name)
+        return self
+    
     def get_resource(self, name: str):
         if name not in self._services:
             raise Exception(f"Resource '{name}' is not registered.")
