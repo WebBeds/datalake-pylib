87650224a6e0b4b22fcb1da96dbb274a4c607adc
Carlos Pomares <cpomaresp@gmail.com>
2022-03-21 13:27:45 +0100

    Added get_schema method

--- sdk/src/datalake/sdk/collection.py
+++ sdk/src/datalake/sdk/collection.py
@@ -8,6 +8,8 @@ class Collection(list):
     _schema: dict = None
     def set_schema(self, schema: dict):
         self._schema = schema
+    def get_schema(self) -> dict:
+        return self._schema
     def to_dataframe(self):
         if self._schema is None:
             return json_normalize(self)
