469ad66f1e4d9285f1d71022387fe1a5246254c4
Carlos Pomares <cpomaresp@gmail.com>
2023-07-20 16:40:09 +0200

    Added lower and force parameters for schema normalize

--- etl/schema/src/datalake/etl/schema/utilities.py
+++ etl/schema/src/datalake/etl/schema/utilities.py
@@ -6,7 +6,15 @@
 
 from pandas import DataFrame
 
-def normalize_df(df: DataFrame, schema, verbose=False) -> DataFrame:
+
+def normalize_df(df: DataFrame, schema, verbose=False, force=False, lower=False) -> DataFrame:
+    if lower:
+        df.columns = df.columns.str.lower()
+
+    if force:
+        available_columns = [c for c in schema.keys() if c in df.columns]
+        df = df[available_columns]
+
     for c in df.columns:
         t = schema.get(c, None)
         if t is None:
