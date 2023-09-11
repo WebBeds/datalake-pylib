3534a247d8992b1e536d537c38f9ef4b1b82a456
Carlos Pomares <cpomaresp@gmail.com>
2023-09-11 10:36:44 +0200

    fix: Allow passing existing connection to Postgres source

--- etl/aws/src/datalake/etl/aws/cloudwatch/monitor/sources/postgres.py
+++ etl/aws/src/datalake/etl/aws/cloudwatch/monitor/sources/postgres.py
@@ -1,27 +1,23 @@
 from dataclasses import dataclass
-from .source import Source, pd
 
 import awswrangler as wr
 import pg8000
 
+from .source import Source, pd
+
+
 @dataclass
 class Postgres(Source):
-
     connection: str
 
-    def retrieve(self, query: str) -> pd.DataFrame:
-        con: pg8000.Connection = wr.postgresql.connect(self.connection)
-        return wr.postgresql.read_sql_query(
-            sql=query,
-            con=con
-        )
+    def retrieve(self, query: str, con: pg8000.Connection = None) -> pd.DataFrame:
+        if not con:
+            con = wr.postgresql.connect(self.connection)
+        return wr.postgresql.read_sql_query(sql=query, con=con)
 
     def parse(data: dict):
-
         connection: str = data.get("options", {}).get("connection", None)
         if not connection:
             raise ValueError("Source must have connection")
-        
-        return Postgres(
-            connection=connection
-        )
+
+        return Postgres(connection=connection)
