d70d761a4774e19500709609548d8e0c0403a073
Carlos Pomares <cpomaresp@gmail.com>
2023-10-18 10:15:19 +0200

    fix: added driver option for sqlserver input

--- etl/aws/src/datalake/etl/aws/cloudwatch/monitor/sources/sqlserver.py
+++ etl/aws/src/datalake/etl/aws/cloudwatch/monitor/sources/sqlserver.py
@@ -8,15 +8,24 @@ from .source import Source, pd
 @dataclass
 class SQLServer(Source):
     connection: str
+    driver: int = 18
 
     def retrieve(self, query: str, con=None) -> pd.DataFrame:
         if not con:
-            con = wr.sqlserver.connect(self.connection)
+            con = wr.sqlserver.connect(
+                self.connection,
+                odbc_driver_version=self.driver,
+            )
         return wr.sqlserver.read_sql_query(sql=query, con=con)
 
     def parse(data: dict) -> "SQLServer":
         connection: str = data.get("options", {}).get("connection", None)
         if not connection:
             raise ValueError("Source must have connection")
-
-        return SQLServer(connection=connection)
+        driver: int = data.get("options", {}).get("driver", 18)
+        if not driver or not isinstance(driver, int):
+            raise ValueError("Source must have driver")
+        return SQLServer(
+            connection=connection,
+            driver=driver,
+        )
