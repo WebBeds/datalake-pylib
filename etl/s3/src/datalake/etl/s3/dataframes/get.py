7765617b8614ddfebdfa4b4ee2a57eff1e88b276
Carlos Pomares <cpomaresp@gmail.com>
2024-04-12 16:06:44 +0200

    fix: checking dataframe before lowering columns

--- etl/s3/src/datalake/etl/s3/dataframes/get.py
+++ etl/s3/src/datalake/etl/s3/dataframes/get.py
@@ -4,10 +4,12 @@
 # DATAFRAMES MODULE         #
 # ========================= #
 
-import boto3
 import io
-import pandas as pd
 from urllib import parse
+
+import boto3
+import pandas as pd
+
 
 def get_parquet_from_s3(bucket, key, verbose=False):
     """
@@ -17,17 +19,21 @@ def get_parquet_from_s3(bucket, key, verbose=False):
 
     # Read file and create dataframe
     try:
-        s3 = boto3.client('s3')
+        s3 = boto3.client("s3")
         obj = s3.get_object(Bucket=bucket, Key=key)
-        df = pd.read_parquet(io.BytesIO(obj['Body'].read()))
+        df = pd.read_parquet(io.BytesIO(obj["Body"].read()))
     except s3.exceptions.NoSuchKey:
         df = pd.DataFrame()
 
     # Colum names to lowercase
-    df.columns = df.columns.str.lower()
+
+    if not df.empty:
+        df.columns = df.columns.str.lower()
+
     return df
 
-def get_csv_from_s3(bucket, key, verbose=False, compression='gzip', keep_default_na=True, sep=','):
+
+def get_csv_from_s3(bucket, key, verbose=False, compression="gzip", keep_default_na=True, sep=","):
     """
     Get CSV from S3 and return it as a Pandas DataFrame.
     """
@@ -36,9 +42,15 @@ def get_csv_from_s3(bucket, key, verbose=False, compression='gzip', keep_default
 
     try:
         # Connect to S3 and get object
-        s3client = boto3.client('s3')
+        s3client = boto3.client("s3")
         obj = s3client.get_object(Bucket=bucket, Key=key)
-        df = pd.read_csv(io.BytesIO(obj['Body'].read()), dtype=str, compression=compression, keep_default_na=keep_default_na, sep=sep)
+        df = pd.read_csv(
+            io.BytesIO(obj["Body"].read()),
+            dtype=str,
+            compression=compression,
+            keep_default_na=keep_default_na,
+            sep=sep,
+        )
     except s3client.exceptions.NoSuchKey:
         df = pd.DataFrame()
 
@@ -46,6 +58,7 @@ def get_csv_from_s3(bucket, key, verbose=False, compression='gzip', keep_default
     df.columns = df.columns.str.lower()
 
     return df
+
 
 def get_json_from_s3(bucket, key, verbose=False) -> pd.DataFrame:
     key = parse.unquote(key)
@@ -58,8 +71,9 @@ def get_json_from_s3(bucket, key, verbose=False) -> pd.DataFrame:
     df.columns = df.columns.str.lower()
     return df
 
+
 def get_excel_from_s3(bucket, key, verbose=False) -> pd.DataFrame:
-    
+
     key = parse.unquote(key)
 
     # Read file and create dataframe
